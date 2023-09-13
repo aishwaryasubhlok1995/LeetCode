6a606a59dc23cfb80e140120a22b15d5a09bb47a
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-09-13 04:21:15 -0700

    Time: 2789 ms (18.43%), Space: 20.8 MB (28.23%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,6 +2,7 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         finalAns = []
+        arr = set()
         for i in range(len(nums)):
             j = i+1
             k = len(nums)-1
@@ -12,11 +13,9 @@ class Solution:
                 if ans<0:
                     j += 1
                 if ans == 0:
-                    arr = [nums[i],nums[j],nums[k]]
-                    if arr not in finalAns:
-                        finalAns.append(arr)
+                    arr.add((nums[i],nums[j],nums[k]))
                     j += 1
-        return finalAns
+        return arr
         
                     
                 
