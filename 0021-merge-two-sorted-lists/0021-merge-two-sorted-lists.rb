c94752f5632260fa56e4924b007720584790f2a4
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-10-17 01:57:23 -0700

    Time: 65 ms (60.70%), Space: 211 MB (47.12%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -1,18 +1,32 @@
-def merge_two_lists(l1, l2)
-    previoushead = ListNode.new(-1)
+=begin
+Question 2
 
-    previous = previoushead
+Solve the following leetcode problem using Ruby language
 
-    while l1 && l2
-        if l1.val<=l2.val
-            previous.next = l1
-            l1 = l1.next
-        else
-            previous.next = l2
-            l2 = l2.next
-        end
-        previous = previous.next
+21. Merge Two Sorted Lists
+
+(submit screen shot of submission and source code file)
+
+=end
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+    return if !list1 && !list2
+    return list1 unless list2
+    return list2 unless list1
+    if list1.val > list2.val
+        list1, list2 = list2, list1
     end
-    previous.next = l1.nil?? l2:l1;
-    previoushead.next
+    list1.next = merge_two_lists(list1.next, list2)
+    list1
 end
