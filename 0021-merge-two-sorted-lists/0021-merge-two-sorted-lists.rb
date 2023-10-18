ba338f8b273cc4c3d15ad5a67233d095d0b03c24
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-10-17 21:51:34 -0700

    Time: 63 ms (69.67%), Space: 211.1 MB (46.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -1,33 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
-# @param {ListNode} list1
-# @param {ListNode} list2
-# @return {ListNode}
 def merge_two_lists(list1, list2)
-    dummy = ListNode.new
-    p = dummy
-    while list1 != nil && list2 != nil
-        if list1.val < list2.val
-            p.next = list1
-            p = list1
-            list1 = list1.next
+   node = pre_res = ListNode.new
+    while list1 && list2
+        if list1.val <= list2.val
+            node.next = list1
+            list1 = list1.next    
         else
-            p.next = list2
-            p = list2
-            list2 = list2.next
+            node.next = list2
+            list2 = list2.next  
         end
+        node = node.next
     end
-        if list1 != nil
-            p.next = list1
-        end
-        if list2 != nil
-            p.next = list2
-        end
-        return dummy.next
+    node.next = list1 || list2
+    pre_res.next 
 end
