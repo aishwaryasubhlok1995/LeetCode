3e157037eab28b3897561215d11a4a32f5e236dc
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-10-20 19:09:35 -0700

    Time: 67 ms (53.62%), Space: 211.2 MB (15.85%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -1,20 +1,22 @@
-def merge_two_lists(l1, l2)
-    dummy = ListNode.new
-    cur = dummy
-    
-    while l1 && l2
-        if l1.val < l2.val
-            cur.next = l1
-            l1 = l1.next
-        else
-            cur.next = l2
-            l2 = l2.next
-        end
-        cur = cur.next
+def merge_two_lists(list1, list2)
+  node = pre_res = ListNode.new(0)
+  while list1 && list2
+    if list1.val <= list2.val
+      node.next = list1
+      list1 = list1.next
+    else
+      node.next = list2
+      list2 = list2.next
     end
-    
-    cur.next = l1 || l2
-    
-    dummy.next
-end
+    node = node.next
+  end
 
+  # Link the remaining elements of the non-empty list
+  if list1.nil?
+    node.next = list2
+  else
+    node.next = list1
+  end
+
+  pre_res.next
+end
