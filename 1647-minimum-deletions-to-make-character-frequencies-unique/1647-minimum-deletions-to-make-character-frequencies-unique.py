f1c3cc58a89626202f357d60e137e38082d0524d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-10-21 22:41:24 -0700

    Time: 2370 ms (5.07%), Space: 18 MB (6.23%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def minDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        arr = set(s)
+        result = []
+        for i in arr:
+            count = 0
+            for j in range(len(s)):
+                if s[j] == i:
+                    count += 1
+            result.append(count)
+        print(result)
+        tempArr = set()
+        res = 0
+        for i in range(len(result)):
+            while result[i] in tempArr:
+                result[i] =  result[i] - 1
+                res += 1
+                if result[i] == 0:
+                    break
+            tempArr.add(result[i])
+        return res
+            
+                    
+                    
+        
+    
+                
+        
