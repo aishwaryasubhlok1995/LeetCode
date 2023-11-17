8a8438ebbdc4717c7a4df5ef9f05463e5ab9e2de
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-11-16 22:15:45 -0800

    Time: 23 ms (29.76%), Space: 13.5 MB (19.29%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        final = ''
+        carry = 0
+        i = len(a)-1
+        j = len(b)-1
+        while i>=0 or j>=0:  
+            if i<0:
+                sum = int(b[j]) + carry
+            elif j<0:
+                sum = int(a[i]) + carry
+            else:
+                sum = int(a[i]) + int(b[j]) + carry
+            if sum > 1:
+                carry = 1
+                if sum == 2:
+                    final = '0' + final 
+                if sum > 2:
+                    final = '1' + final 
+            else:
+                final = str(sum) + final 
+                carry = 0
+            i -= 1
+            j -= 1
+            
+        if carry == 1:
+            final = '1' + final
+        return final
+            
+        
+        
