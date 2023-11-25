5c7efe040baf57a1413372a7e1771daac74e9063
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2023-11-25 01:29:46 -0800

    Time: 89 ms (8.02%), Space: 16.6 MB (35.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        arr = list(magazine)
-        for i in range (len(ransomNote)):
-            if ransomNote[i] in arr:
-                arr.remove(ransomNote[i])
+        dictMagazine = {}
+        for i in range(len(magazine)):
+            if magazine[i] in dictMagazine.keys():
+                dictMagazine[magazine[i]]  += 1
+            else:
+                dictMagazine[magazine[i]] = 1
+        for i in range(len(ransomNote)):
+            if ransomNote[i] in dictMagazine.keys():
+                dictMagazine[ransomNote[i]] -= 1
+                if dictMagazine[ransomNote[i]] == 0:
+                    dictMagazine.pop(ransomNote[i])
             else:
                 return False
         return True
