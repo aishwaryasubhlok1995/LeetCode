b0e81a2af32b5e2ef43f510e26b8f4477491cc1c
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-03-26 02:10:02 -0700

    Time: 271 ms (51.81%), Space: 18.2 MB (58.37%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,45 +1,22 @@
+import math
 class Solution:
-    import math
-    
-    def calculatehours(self, mid, piles):
-        total = 0
-        if mid>0:
-            for i in piles:
-                total += math.ceil(i/mid)
-        return total
-        
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        '''the whole idea is to find k based on h and len of piles, have to develop a logic to return k, refer page 74 of the diary'''
-        lenPiles = len(piles)
-        maxElement = max(piles)
-        if lenPiles == h:
-            return maxElement
-        if lenPiles < h:
-            '''binary search'''
-            first = 1
-            last = maxElement
-            while first<last:
-                mid = (first + last)//2
-                print(first)
-                print(last)
-                total = self.calculatehours(mid, piles)
-                while self.calculatehours(mid-1, piles) == h:
-                    total = self.calculatehours(mid-1, piles)
-                    mid = mid -1 
-                if total == h:
-                    return mid
-                if total < h:
-                    last = mid - 1
-                if total > h:
-                    first = mid + 1
-            while total > h:
-                total = self.calculatehours(mid+1, piles)
-                mid = mid + 1
-            while total < h and self.calculatehours(mid-1, piles) < h and mid>1:
-                total = self.calculatehours(mid+1, piles)
-                mid = mid - 1
-            return mid
-                
-                
+    def returnHours(self, piles, mid):
+        hours=0
+        for p in piles:
+            hours = hours + math.ceil(p/mid)
+        return hours
             
-        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        high=max(piles)
+        if h==len(piles):
+            return high
+        low=1
+        while low<high:
+            mid = (high + low) // 2
+            hours = self.returnHours(piles, mid)
+            if hours <= h:
+                high=mid
+            if hours > h:
+                low=mid+1
+        print(high)
+        return high
