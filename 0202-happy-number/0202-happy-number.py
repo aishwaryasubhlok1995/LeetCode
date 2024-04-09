619959852281f33e293009a90ed65a2665d0f002
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-08 17:53:29 -0700

    Time: 27 ms (97.23%), Space: 16.5 MB (39.00%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,21 +1,24 @@
 class Solution:
     import math
-    def isHappy(self, n: int) -> bool:
-        tempList = set()
-        sumOfdigits = 0 
+    def getSquareOfdigit(self, n):
+        sumOfdigits = 0
         while n>0:
             number = n%10
             sumOfdigits += number*number
             n = n//10
-            if n == 0:
-                beforeLength = len(tempList)
-                if sumOfdigits == 1:
-                    return True
-                tempList.add(sumOfdigits)
-                if beforeLength == len(tempList):
-                    return False
-                n = sumOfdigits
-                sumOfdigits = 0
+        return sumOfdigits
+        
+    def isHappy(self, n: int) -> bool:
+        tempList = set()
+        while n>0:
+            sumOfdigits = self.getSquareOfdigit(n)
+            beforeLength = len(tempList)
+            if sumOfdigits == 1:
+                return True
+            tempList.add(sumOfdigits)
+            if beforeLength == len(tempList):
+                return False
+            n = sumOfdigits
                 
         
             
