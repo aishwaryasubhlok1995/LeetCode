4022b09e47948d67c27075616515909c3464aa4d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-15 21:49:19 -0700

    Time: 304 ms (21.12%), Space: 18.2 MB (57.84%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,28 @@
-import math
 class Solution:
-    def returnHours(self, piles, mid):
-        hours=0
-        for p in piles:
-            hours = hours + math.ceil(p/mid)
-        return hours
-            
+    def calculateFromPiles(self, piles, k):
+        count = 0 
+        for i in range(len(piles)):
+            count += ceil(piles[i]/k)
+        return count
+    
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        high=max(piles)
-        if h==len(piles):
-            return high
-        low=1
-        while low<high:
-            mid = (high + low) // 2
-            hours = self.returnHours(piles, mid)
-            if hours <= h:
-                high=mid
-            if hours > h:
-                low=mid+1
-        return high
+        minimumKElement = math.inf
+        piles = sorted(piles)
+        if len(piles) == h:
+            return max(piles)
+#         Apply binary search 
+        i = 1
+        j = max(piles)
+        while i<=j:
+            mid = (i+j)//2
+            if self.calculateFromPiles(piles, mid) <= h:
+                minimumKElement = min(mid, minimumKElement)
+                j =  mid-1
+            else:
+                i = mid+1
+        return minimumKElement
+            
+            
+            
+        
+        
