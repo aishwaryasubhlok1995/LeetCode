e1f2aabaff8a9dd1d40694bd213d2c060e249a0d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-23 16:00:46 -0700

    Time: 30 ms (87.24%), Space: 16.6 MB (70.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,12 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n < 0:
-            return 1/(x * self.myPow(x, -n-1))
+            return 1/ self.myPow(x, -n)
         if n == 0:
             return 1
         if n%2 == 0:
-            return self.myPow(x*x, n/2)
-        return x*self.myPow(x*x, (n-1)/2)
+            return self.myPow(x*x, n//2)
+        else:
+            return x * self.myPow(x,n-1)
         
             
         
