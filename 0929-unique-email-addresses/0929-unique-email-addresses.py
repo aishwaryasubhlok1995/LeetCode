ff573c9f46bc514d6127f4959b323f5b46451246
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-24 21:56:55 -0700

    Time: 49 ms (86.65%), Space: 16.7 MB (25.27%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(emails)):
             strinEmails = emails[i].split('@')
             stringFirstHalf  = strinEmails[0].split('+')[0].replace('.', '')
-            element =  stringFirstHalf + '@' + strinEmails[1]
-            ans.add(element)
+            ans.add(stringFirstHalf + '@' + strinEmails[1])
         return len(ans)
         
