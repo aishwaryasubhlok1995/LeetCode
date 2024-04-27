ec414f3bb10ff5059cd14bf882fa4c0246c22063
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-27 14:52:28 -0700

    Time: 128 ms (34.08%), Space: 20.7 MB (37.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,7 +4,6 @@ class Solution:
         listIndexesToRemove = []
         i =1
         while i < len(intervals):
-            print(intervals[i], intervals[i-1])
             if intervals[i-1][1] >= intervals[i][0]:
                 end = intervals[i][1] if intervals[i][1] >intervals[i-1][1] else intervals[i-1][1]
                 intervals[i] = [intervals[i-1][0], end]
