51b55c692d4dead4c5b4fc0136bd5dadde43dae0
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-30 01:33:13 -0700

    Time: 313 ms (10.67%), Space: 24.2 MB (26.90%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,28 @@
-class Solution:
-     
-    def bfs(self, r, c, visited, grid):
-        directions = [[1,0],[-1, 0], [0, 1], [0, -1]]
-        queuelist = deque()
-        queuelist.append([r,c])
-        visited.add((r,c))
-        while queuelist:
-            curr = queuelist.popleft()
-            for d in (directions):
-                row = curr[0]+d[0]
-                column = curr[1]+d[1]
-                if (row in range(len(grid))) and (column in range(len(grid[0]))) and (row,column) not in visited and grid[row][column] == '1':
-                    queuelist.append([row, column])
-                    visited.add((row, column))
-        
-    
+class Solution:    
     def numIslands(self, grid: List[List[str]]) -> int:
         island = 0
         if grid is None:
             return 
         visited = set()  
+
+        def bfs( r, c):
+            directions = [[1,0],[-1, 0], [0, 1], [0, -1]]
+            queuelist = deque()
+            queuelist.append([r,c])
+            visited.add((r,c))
+            while queuelist:
+                curr = queuelist.popleft()
+                for d in (directions):
+                    row = curr[0]+d[0]
+                    column = curr[1]+d[1]
+                    if (row in range(len(grid))) and (column in range(len(grid[0]))) and (row,column) not in visited and grid[row][column] == '1':
+                        queuelist.append([row, column])
+                        visited.add((row, column))
         
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == '1' and (r,c) not in visited:
-                    self.bfs(r,c, visited, grid)
+                    bfs(r,c)
                     island += 1
         return island
                 
