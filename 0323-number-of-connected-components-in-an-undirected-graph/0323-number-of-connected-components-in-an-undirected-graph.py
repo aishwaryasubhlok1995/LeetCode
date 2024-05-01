af474991ad31520195d0bff7dc0f6d1925e87e48
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-04-30 18:37:17 -0700

    Time: 89 ms (66.21%), Space: 18.3 MB (77.17%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        def bfs(i):
+            print(i)
+            visited[i] = True
+            for node in (adj[i]):
+                if visited[node] == False:
+                    bfs(node)
+                
+            
+        
+        adj = [[] for i in range(n)]
+        visited = [False]*n
+        for i in edges:
+            adj[i[0]].append(i[1])
+            adj[i[1]].append(i[0])
+        counter = 0
+        for i in range(len(adj)):
+            if visited[i] == False:
+                counter += 1
+                bfs(i)
+        return counter
+            
+        
