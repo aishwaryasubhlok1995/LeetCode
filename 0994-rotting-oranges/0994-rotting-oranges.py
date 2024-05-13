af15cf5fc699610b2022f5c2f726b39159833f7b
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-12 18:35:36 -0700

    Time: 55 ms (28.50%), Space: 16.8 MB (8.60%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,39 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        visited = set()
+        queueList = deque()
+        newList = deque()
+        ans = 0
+        containsfresh = False
+        directions = [[-1, 0], [1,0], [0, -1], [0, 1]]                
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visited and grid[i][j] == 2:
+                    queueList.append([i, j])
+                    visited.add((i, j))
+        print(queueList)
+        while queueList:
+            element = queueList.popleft()
+            for position in directions:
+                leftPosition = element[0] + position[0]
+                rightPosition = element[1] + position[1]
+                if leftPosition in range(len(grid)) and rightPosition in range(len(grid[0])) and (leftPosition, rightPosition) not in visited and grid[leftPosition][rightPosition] == 1:
+                    grid[leftPosition][rightPosition] = 2
+                    visited.add((leftPosition, rightPosition))
+                    newList.append([leftPosition, rightPosition])
+            if len(queueList) == 0:
+                if len(newList) > 0 : 
+                    queueList = newList
+                    newList = deque()
+                    ans += 1
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if ((i,j) not in visited and grid[i][j] == 1):
+                    return -1
+        return ans
+
+        
+        
+                    
+        
+            
