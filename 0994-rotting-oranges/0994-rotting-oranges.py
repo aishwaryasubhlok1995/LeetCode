12e94d74e3fe45a7a76b86b61795ca6024cd23f8
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-12 18:57:42 -0700

    Time: 47 ms (76.56%), Space: 16.7 MB (25.10%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,19 +4,24 @@ class Solution:
         queueList = deque()
         newList = deque()
         ans = 0
+        count = 0
         containsfresh = False
+        fresh = 0
         directions = [[-1, 0], [1,0], [0, -1], [0, 1]]                
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if (i,j) not in visited and grid[i][j] == 2:
                     queueList.append([i, j])
                     visited.add((i, j))
+                if grid[i][j] == 1:
+                    fresh += 1
         while queueList:
             element = queueList.popleft()
             for position in directions:
                 leftPosition = element[0] + position[0]
                 rightPosition = element[1] + position[1]
                 if leftPosition in range(len(grid)) and rightPosition in range(len(grid[0])) and (leftPosition, rightPosition) not in visited and grid[leftPosition][rightPosition] == 1:
+                    count += 1
                     grid[leftPosition][rightPosition] = 2
                     visited.add((leftPosition, rightPosition))
                     newList.append([leftPosition, rightPosition])
@@ -25,12 +30,9 @@ class Solution:
                     queueList = newList
                     newList = deque()
                     ans += 1
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if ((i,j) not in visited and grid[i][j] == 1):
-                    return -1
+        if fresh != count:
+            return -1
         return ans
-
         
         
                     
