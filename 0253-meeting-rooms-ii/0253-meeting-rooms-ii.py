955ca77dda8984d066c61ad1f71ae9b276ae59b5
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-20 23:08:25 -0700

    Time: 80 ms (22.95%), Space: 19.8 MB (83.16%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -8,7 +8,6 @@ class Solution:
         heapq.heapify(listOfAvailbilty)
         for i in range(1, len(intervals)): 
             ans = heapq.nsmallest(1, listOfAvailbilty)
-            print(intervals[i][1], ans[0])
             if intervals[i][0] < ans[0]:
                 countOfRooms += 1
                 heapq.heappush(listOfAvailbilty, intervals[i][1])
