c68b7f2d17eb87ea31c997da58de25cb375988ea
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-26 16:25:43 -0700

    Time: 161 ms (12.84%), Space: 17 MB (13.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,24 +2,30 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         i = 0
         j = 0
-        dictChar = {s[0]:1}
+        dictChar = {s[j]:1}
         maxLen = 1
-        while j <len(s)-1 :
-            if (j-i+1-max(dictChar.values())) <= k:
-                j = j+1
+        sumOccurence = 0
+        while j<len(s)-1:
+            if sumOccurence <= k:
+                maxLen = max(maxLen, (j -i +1))
+                j += 1
                 if s[j] not in dictChar:
                     dictChar[s[j]] = 1
                 else:
-                    dictChar[s[j]] += 1 
+                    dictChar[s[j]] += 1
             else:
+                print(dictChar[s[i]])
                 if dictChar[s[i]] > 0:
-                    dictChar[s[i]] = dictChar[s[i]] - 1
+                    dictChar[s[i]] =dictChar[s[i]] -1
                 else:
                     del dictChar[s[i]]
-                i += 1
-            if (j-i+1-max(dictChar.values())) <= k:
-                maxLen = max(j-i+1, maxLen)
+                i += 1 
+            maxaVal = max(dictChar.values())
+            sumOccurence = j -i +1-maxaVal
+        if sumOccurence <= k:
+            maxLen = max(maxLen, (j -i +1))
         return maxLen
+                
                 
                 
             
