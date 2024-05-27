ad15605caf4132ef3af3f03f0202880417d885dc
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-26 23:02:48 -0700

    Time: 36 ms (49.00%), Space: 16.4 MB (96.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,8 +5,7 @@ class Solution:
         if n == 0:
             return 1
         if n < 0:
-            n = n *-1
-            x = 1/x
+            return 1/ self.myPow(x, -n)
         if n%2 == 0:
             return self.myPow(x*x, n//2)
         else:
