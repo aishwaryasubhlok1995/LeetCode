7ddfec9746c4ca7d2a42803bb2bee9c4877c9b45
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-28 10:38:53 -0700

    Time: 44 ms (6.36%), Space: 16.4 MB (99.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,13 +1,15 @@
+import heapq
 class Solution:
-    import heapq
     def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] = stones[i]*-1 #workaround to get largest in priority queue
         heapq.heapify(stones)
-        while len(stones) > 1:
-            temp = nlargest(2, stones)
-            if temp[0] != temp[1]:
-                heapq.heappush(stones, temp[0]-temp[1])
-            stones.remove(temp[0])
-            stones.remove(temp[1])
-        if len(stones) > 0:
-            return stones[0]
+        while len(stones)>1:
+            s1 = heapq.heappop(stones)
+            s2 = heapq.heappop(stones)
+            res = s1-s2
+            if res!=0:
+                heapq.heappush(stones,res)
+        if len(stones)!=0:
+            return -1*stones[0]
         return 0
