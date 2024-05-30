866d898535f6f5ccb403e41ea89ec10e36e279ff
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-29 18:18:39 -0700

    Time: 40 ms (87.73%), Space: 17.1 MB (75.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,30 +1,26 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        low = 0
-        high = len(matrix)-1
-        while low <= high:
-            middle = (low + high)//2 
-            lowValue = matrix[middle][0]
-            highValue = matrix[middle][-1]
-            if target>= lowValue and target <= highValue:
-                '''binary search for column'''
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i = 0
+        j = len(matrix)-1
+        while i<=j:
+            mid = (i+j)//2
+            if target>= matrix[mid][0] and target<= matrix[mid][-1]:
                 start = 0
-                end = len(matrix[middle])-1
-                while(start<=end):
-                    middlElement = (start+end)//2
-                    if target == matrix[middle][middlElement]:
+                end = len(matrix[0]) - 1
+                while start<=end:
+                    middle = (start+end)//2
+                    print(start)
+                    print(end)
+                    print(middle)
+                    if target == matrix[mid][middle]:
                         return True
-                    elif target < matrix[middle][middlElement]:
-                        end = middlElement - 1
-                    elif target > matrix[middle][middlElement]:
-                        start = middlElement + 1
+                    if target < matrix[mid][middle]:
+                        end = middle -1
+                    elif target > matrix[mid][middle]:
+                        start = middle +1
                 return False
-            elif target< lowValue:
-                high = middle - 1
-            elif target> highValue:
-                low = middle + 1
+            elif target<matrix[mid][0]:
+                j = mid-1
+            elif target>matrix[mid][-1]:
+                i = mid+1
+        return False
