8f59b339f92a0f63c15c2e8efe2d25368a630fe8
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-05-30 22:04:16 -0700

    Time: 98 ms (22.69%), Space: 16.5 MB (92.94%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #explore i and j for i to 3, and j to j+3
+            #initially keep i stable and jsut extend j 
+            # when j reaches 9- #increase I and make j to 0 
+            
+        visitedI = set()
+        vistedJ = set()
+        visitedDict = {}
+        i = 0 
+        j = 0 
+        for i in range(0, 9):
+            visitedI = set()
+            vistedJ = set()
+            for j in range(0, 9):
+                if board[i][j] != '.':
+                    if board[i][j] not in visitedI:
+                        visitedI.add(board[i][j])
+                    else:
+                        return False
+                    key = (3*(i//3) + (j//3))
+                    if key not in visitedDict:
+                        visitedDict[key] = set(board[i][j])
+                    else:
+                        keySet = visitedDict[key]
+                        if board[i][j] in keySet:
+                            return False
+                        visitedDict[key].add(board[i][j])
+                if board[j][i] != '.':
+                    if board[j][i] not in vistedJ:
+                        vistedJ.add(board[j][i])
+                    else:
+                        return False
+                    
+        return True
+                    
+                
+                
+                            
+            
+        
