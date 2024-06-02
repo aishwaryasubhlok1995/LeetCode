5c9a5d4d590afecf66e146ad36b08ca5804fa488
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-01 22:26:08 -0700

    Time: 5616 ms (5.01%), Space: 16.6 MB (95.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,3 @@
-# class Solution:
-#     def checkInclusion(self, s1: str, s2: str) -> bool:
-#         dictS1 = {}
-#         for i in s1:
-#             if i not in dictS1:
-#                 dictS1[i]  = 1
-#             else:
-#                 dictS1[i] += 1
-#         tempDict = dictS1.copy()
-#         for i in range(len(s2)):
-#             if s2[i] in tempDict:
-#                 for k in range(len(s1)):
-#                     tempDict[s2[i]] -= 1
-#                     if tempDict[s2[i]] == 0:
-#                         del tempDict[s2[i]]
-#                     if tempDict == {}:
-#                         return True 
-#                     if i+1 <len(s2) and s2[i+1] in tempDict:
-#                         i = i+1
-#                     else:
-#                         tempDict = dictS1.copy()
-#                         break
-#         return False 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         dictS1 = {}
@@ -42,6 +19,4 @@ class Solution:
                     tempDict = dictS1.copy()
                     break
         return False 
-            
-        
-        
+    
