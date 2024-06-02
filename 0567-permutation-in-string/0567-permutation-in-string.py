81cb1d68e3e15b756aa633b2305cb43bade85190
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-01 22:44:46 -0700

    Time: 4696 ms (5.01%), Space: 16.6 MB (74.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,16 +8,14 @@ class Solution:
                 dictS1[i] += 1
         tempDict = dictS1.copy()
         for i in range(len(s2)-len(s1)+1):
-            if s2[i] in tempDict:
-                for k in range(len(s1)):
-                    tempDict[s2[i]] -= 1
-                    if tempDict[s2[i]] == 0:
-                        del tempDict[s2[i]]
+            for k in range(len(s1)):
+                if s2[i+k] in tempDict:
+                    tempDict[s2[i+k]] -= 1
+                    if tempDict[s2[i+k]] == 0:
+                        del tempDict[s2[i+k]]
                     if tempDict == {}:
-                        return True 
-                    if i+1 <len(s2) and s2[i+1] in tempDict:
-                        i = i+1
-                    else:
-                        tempDict = dictS1.copy()
-                        break
+                        return True  
+                else:
+                    tempDict = dictS1.copy()
+                    break
         return False 
