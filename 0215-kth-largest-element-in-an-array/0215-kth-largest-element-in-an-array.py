6ce23a386835e4e19c962492eb4f2ddab7c87828
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-02 23:16:16 -0700

    Time: 491 ms (31.63%), Space: 29.7 MB (70.32%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,6 +4,7 @@ class Solution:
         arr = []
         for i in range(len(nums)):
             heapq.heappush(arr, nums[i])
+            
             if len(arr) > k:
                 heapq.heappop(arr)
         return heapq.heappop(arr)
