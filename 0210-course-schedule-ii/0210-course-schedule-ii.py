68cf4f3db61542d057fbd11c54b184a442f2ef7a
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-04 01:20:58 -0700

    Time: 84 ms (87.35%), Space: 17.9 MB (99.32%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,28 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        ans = []
-        queList = deque()
         adj = [[] for i in range(numCourses)]
+        ans = []
+        queuelist = deque()
         for i in prerequisites:
             adj[i[1]].append(i[0])
-        indegree = [0]*numCourses
+        print(adj)
+        #calculate indegrees
+        indegrees = [0]*numCourses
         for i in adj:
-            for j in i:
-                indegree[j] +=  1
-        for i in range(len(indegree)):
-            if indegree[i] ==0:
-                queList.append(i)
-        while queList:
-            node = queList.popleft()
+            for j in range(len(i)):
+                indegrees[i[j]] += 1
+        print(indegrees)
+        for i in range(numCourses):
+            if indegrees[i] == 0:
+                queuelist.append(i)
+        while queuelist:
+            node = queuelist.popleft()
             ans.append(node)
-            for i in (adj[node]):
-                indegree[i] -= 1
-                if indegree[i] == 0:
-                    queList.append(i)
+            for i in adj[node]:
+                indegrees[i] -= 1
+                if indegrees[i] == 0:
+                    queuelist.append(i)
         if len(ans) != numCourses:
             return []
         return ans
         
-
-            
-            
+        
+        
