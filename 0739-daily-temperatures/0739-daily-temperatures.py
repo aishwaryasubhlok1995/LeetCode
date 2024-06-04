8dc5dea55f8724911628a646d53cdfc0157cd31f
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-03 23:08:38 -0700

    Time: 943 ms (10.63%), Space: 32.6 MB (14.91%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         ans = [0]*len(temperatures)
-        stackArr = []
-        for i in range(len(temperatures)):
+        stackArr = [[temperatures[0], 0]]
+        for i in range(1, len(temperatures)):
+            while len(stackArr) > 0 and temperatures[i] > stackArr[-1][0]:
+                ans[stackArr[-1][1]] = i - stackArr[-1][1]
+                stackArr.pop(-1)
             stackArr.append([temperatures[i], i])
-            if len(stackArr) >= 2:
-                i = -2
-                while abs(i) <= len(stackArr):
-                    if stackArr[i][0] < stackArr[-1][0]:
-                        ans[stackArr[i][1]] = stackArr[-1][1] - stackArr[i][1]
-                        stackArr.pop(i)
-                    else:
-                        break
+
         return ans
                 
             
