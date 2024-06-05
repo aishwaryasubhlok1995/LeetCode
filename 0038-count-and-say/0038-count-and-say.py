c03c7e785e46dcf3cef2f31c35d84a1e5ace2264
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-04 21:25:04 -0700

    Time: 36 ms (90.72%), Space: 16.7 MB (21.35%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,18 +2,15 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return '1'
-        arr = []
         ans = ''
         no = self.countAndSay(n-1)
+        arr = [[no[0], 1]]
         if n >= 2:
-            for i in range(len(no)):
-                if len(arr) == 0:
-                    arr.append([no[i], 1])
+            for i in range(1, len(no)):
+                if arr[-1][0] == no[i]:
+                    arr[-1][1] += 1
                 else:
-                    if arr[-1][0] == no[i]:
-                        arr[-1][1] += 1
-                    else:
-                        arr.append([no[i], 1])
+                    arr.append([no[i], 1])
             for i in range(len(arr)):
                 ans +=  str(arr[i][1]) + str(arr[i][0]) 
             return ans
