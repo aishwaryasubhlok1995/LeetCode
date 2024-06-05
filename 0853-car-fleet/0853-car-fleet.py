483d5d3db521fea730f07720b3e89a6816398da0
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-05 02:36:07 -0700

    Time: 950 ms (5.00%), Space: 39.9 MB (14.44%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #car who is ahead should be slower than car who is behind but faster = together fleet 
+        #car who is behind but even taking longer duration - they are alone 
+        newPositions = []
+        for i in range(len(position)):
+            newPositions.append([position[i], speed[i]])
+        newPositions = sorted(newPositions)
+        newPositions = newPositions[::-1]
+        print(newPositions)
+        arr = [(target - newPositions[0][0])/newPositions[0][1]]
+        for i in range(1, len(newPositions)):
+            time = (target - newPositions[i][0])/newPositions[i][1]
+            if time > arr[-1]:
+                arr.append(time)
+        return len(arr)
+            
+                
+            
+            
+            
+            
+        
