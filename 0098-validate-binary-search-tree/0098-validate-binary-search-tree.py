ba8a5051f31ffe0db421b13b8cd2dbf0148075a6
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-19 17:58:47 -0700

    Time: 48 ms (17.01%), Space: 18.4 MB (35.29%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,26 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        maxRange = math.inf
-        minRange = -math.inf
-        def validateBinaryTree(root, minRange, maxRange):
-            if root == None:
-                return True
-            if root.val <= minRange or root.val >= maxRange:
-                return False
-            return validateBinaryTree(root.left, minRange, root.val) and validateBinaryTree(root.right, root.val, maxRange)
+class Solution:
+    def checkLimits(self, root, minVal, maxVal):
+        if root == None:
+            return True
+        if root.val <= minVal or root.val >= maxVal:
+            return False
+        return self.checkLimits(root.left, minVal, root.val) and self.checkLimits(root.right, root.val, maxVal)
         
-        return validateBinaryTree(root, minRange, maxRange)
-       
-            
-            
-                
+        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        minVal = -math.inf
+        maxVal = math.inf
+        if root == None:
+            return 
+        return self.checkLimits(root, minVal, maxVal)
