c32efc77752babfca5b0b94e8c0d9bf0217838f4
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-21 16:12:28 -0700

    Time: 28 ms (96.90%), Space: 16.5 MB (83.75%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,11 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num2 = int(num2)
+        prod1 = 0
+        prod2 = 0
+        if len(num1) == 1 and len(num2) == 1:
+            return str(int(num1)*int(num2))
+        if len(num1) < len(num2):
+            prod1 = num1
+            prod2 = int(num2)
+        else:
+            prod1 = num2
+            prod2 = int(num1)
         ans = 0
-        num1 = num1[::-1]
-        for i in range(len(num1)):
-            ans += num2 * (int(num1[i]) * 10**i)
+        for i in range(len(prod1)):
+            ans += prod2 * (int(prod1[i]) * 10 **(len(prod1) - i-1))
         return str(ans)
-       
-            
-        
