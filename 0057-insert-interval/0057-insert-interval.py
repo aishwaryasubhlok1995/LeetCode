191c0b2ce2378367bdbeeae2e84e2720a57febb4
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-22 21:48:18 -0700

    Time: 77 ms (44.62%), Space: 20.2 MB (5.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,26 +1,19 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        ans = []
-        isNewinterval = False
+        if not len(intervals):
+            return [newInterval]
         for i in range(len(intervals)):
-            if  newInterval[0] >= intervals[i][0] and newInterval[0] <= intervals[i][1]:
-                isNewinterval = True
-                if newInterval[1] > intervals[i][1]:
-                    newInterval = [intervals[i][0], newInterval[1]]
-                else:
-                    newInterval = [intervals[i][0], intervals[i][1]]
-            elif newInterval[0] <= intervals[i][0] and  newInterval[1] >= intervals[i][1]:
-                continue
-            elif newInterval[1] >= intervals[i][0] and  newInterval[1] <= intervals[i][1]:
-                isNewinterval = True
-                if newInterval[0] < intervals[i][0]:
-                    newInterval = [newInterval[0], intervals[i][1]]
-                else:
-                    newInterval = [intervals[i][0], intervals[i][1]]   
+            if newInterval[0] <= intervals[i][0]:
+                intervals.insert(i, newInterval)
+                break
+            if i == len(intervals)-1:
+                intervals.insert(i+1, newInterval)
+        print(intervals)
+        ans = []
+        ans.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= ans[-1][1]:
+                ans[-1] = [min(intervals[i][0], ans[-1][0]), max(intervals[i][1], ans[-1][1])]
             else:
-                ans.append([intervals[i][0], intervals[i][1]])
-        ans.append(newInterval)
-        return sorted(ans)
-                    
-                
-        
+                ans.append(intervals[i])
+        return  ans            
