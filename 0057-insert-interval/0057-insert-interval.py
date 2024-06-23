4c02888fc28a4e04e42931184315ca5b1c59633d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-22 21:51:03 -0700

    Time: 75 ms (56.82%), Space: 19.9 MB (61.54%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -12,7 +12,8 @@ class Solution:
         ans.append(intervals[0])
         for i in range(1, len(intervals)):
             if intervals[i][0] <= ans[-1][1]:
-                ans[-1] = [min(intervals[i][0], ans[-1][0]), max(intervals[i][1], ans[-1][1])]
+                if intervals[i][1] >ans[-1][1]:
+                    ans[-1][1] = intervals[i][1]
             else:
                 ans.append(intervals[i])
         return  ans            
