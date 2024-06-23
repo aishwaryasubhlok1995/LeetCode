c462dbaeb013d4faabbce140a715171001185873
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-22 19:04:05 -0700

    Time: 72 ms (74.18%), Space: 19.9 MB (61.54%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,22 +1,24 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
-        for i in intervals:
-            if  newInterval[0] >= i[0] and newInterval[0] <= i[1]:
-                if newInterval[1] > i[1]:
-                    newInterval = [i[0], newInterval[1]]
+        isNewinterval = False
+        for i in range(len(intervals)):
+            if  newInterval[0] >= intervals[i][0] and newInterval[0] <= intervals[i][1]:
+                isNewinterval = True
+                if newInterval[1] > intervals[i][1]:
+                    newInterval = [intervals[i][0], newInterval[1]]
                 else:
-                    newInterval = [i[0], i[1]]
-            elif newInterval[0] <= i[0] and  newInterval[1] >= i[1]:
-                    continue
-            elif newInterval[1] >= i[0] and  newInterval[1] <= i[1]:
-                if newInterval[0] < i[0]:
-                    newInterval = [newInterval[0], i[1]]
+                    newInterval = [intervals[i][0], intervals[i][1]]
+            elif newInterval[0] <= intervals[i][0] and  newInterval[1] >= intervals[i][1]:
+                continue
+            elif newInterval[1] >= intervals[i][0] and  newInterval[1] <= intervals[i][1]:
+                isNewinterval = True
+                if newInterval[0] < intervals[i][0]:
+                    newInterval = [newInterval[0], intervals[i][1]]
                 else:
-                    newInterval = [i[0], i[1]]
-                
+                    newInterval = [intervals[i][0], intervals[i][1]]   
             else:
-                ans.append([i[0],i[1]])
+                ans.append([intervals[i][0], intervals[i][1]])
         ans.append(newInterval)
         return sorted(ans)
                     
