311a7137054acb6d1041394dc6cf4d187f07eb32
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-24 00:06:59 -0700

    Time: 66 ms (62.03%), Space: 16.8 MB (34.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,22 +2,21 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         dir = [[0,1], [1, 0], [-1, 0], [0, -1]]
         Originalcolor = image[sr][sc]
+        rowLen = len(image)
+        colLen = len(image[0])
         visited = set()
         quelist = deque()
-        if Originalcolor != color:
-            quelist.append([sr, sc])
-            visited.add((sr, sc))
-            image[sr][sc] = color
-        else:
-            return image
+        quelist.append([sr, sc])
+        visited.add((sr, sc))
+        image[sr][sc] = color
         while quelist:
             element = quelist.popleft()
             for i in dir:
                 newRow = (element[0] + i[0])
                 newColumn = (element[1] + i[1]) 
-                if newRow in range(len(image)) and newColumn in range(len(image[0])) and (newRow,newColumn) not in visited and image[newRow][newColumn] == Originalcolor:        
+                if newRow in range(rowLen) and newColumn in range(colLen) and (newRow,newColumn) not in visited and image[newRow][newColumn] == Originalcolor:        
                     quelist.append([newRow,newColumn])
-                    visited.add((sr, sc))
+                    visited.add((newRow, newColumn))
                     image[newRow][newColumn] = color
         return image
                     
