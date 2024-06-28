ce425df73552a105607e987085a6eb446a27cd50
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-28 01:03:11 -0700

    Time: 726 ms (18.22%), Space: 24.6 MB (13.59%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -12,13 +12,12 @@ class Solution:
                 dictFruits[fruits[j]] = 1
             else:
                 dictFruits[fruits[j]] += 1
-            if len(dictFruits) > 2:
-                while len(dictFruits) > 2:
-                    if dictFruits[fruits[i]] >= 1:
-                        dictFruits[fruits[i]] -= 1
-                    if dictFruits[fruits[i]] == 0:
-                        del dictFruits[fruits[i]]
-                    i = i+1
+            while len(dictFruits) > 2:
+                if dictFruits[fruits[i]] >= 1:
+                    dictFruits[fruits[i]] -= 1
+                if dictFruits[fruits[i]] == 0:
+                    del dictFruits[fruits[i]]
+                i = i+1
             maxCount = max(maxCount, j-i+1)
         if maxCount == 0:
             return 1
