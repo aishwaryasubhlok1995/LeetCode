132ea0bb33c492daddd7417d52c7ca63fb43253b
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-06-28 03:01:44 -0700

    Time: 45 ms (9.38%), Space: 16.4 MB (93.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,30 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        dictMap = {}
+        dictMap = {0:0, 1:0, 2:0}
+        for i in nums:
+            dictMap[i] += 1
         for i in range(len(nums)):
-            if nums[i] not in dictMap:
-                dictMap[nums[i]] = 1
-            else:
-                dictMap[nums[i]] += 1
-        i = 0
-        while i < len(nums):
-            if 0 in dictMap:
-                dictMap[0] -= 1 
+            if i < dictMap[0]:
                 nums[i] = 0
-                if dictMap[0] == 0:
-                    del dictMap[0]
-            elif 1 in dictMap:
-                dictMap[1] -= 1 
+            elif i < dictMap[1] + dictMap[0]:
                 nums[i] = 1
-                if dictMap[1] == 0:
-                    del dictMap[1]
-            elif 2 in dictMap:
-                dictMap[2] -= 1 
+            elif i < dictMap[2] + dictMap[1] + dictMap[0]:
                 nums[i] = 2
-                if dictMap[2] == 0:
-                    del dictMap[2]
-            i = i+1
+        
+                
+            
             
                 
                 
