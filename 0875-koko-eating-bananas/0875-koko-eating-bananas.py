f6ef33d7dd5255b8e8a22158185892d00823ef55
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-07-11 02:18:29 -0700

    Time: 371 ms (6.07%), Space: 18.1 MB (74.73%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,28 +1,27 @@
 class Solution:
-    def calculateFromPiles(self, piles, k):
-        count = 0 
-        for i in range(len(piles)):
-            count += ceil(piles[i]/k)
-        return count
-    
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        minimumKElement = math.inf
-        piles = sorted(piles)
-        if len(piles) == h:
-            return max(piles)
-#         Apply binary search 
+        k = max(piles)
+        if h == len(piles):
+            return k
         i = 1
-        j = max(piles)
+        j = k
+        minRate = math.inf
         while i<=j:
+            bananaRate = 0
             mid = (i+j)//2
-            if self.calculateFromPiles(piles, mid) <= h:
-                minimumKElement = min(mid, minimumKElement)
-                j =  mid-1
+            for pile in range(len(piles)):
+                if piles[pile] > mid:
+                    bananaRate += (piles[pile])//mid 
+                    if (piles[pile])%mid > 0:
+                        bananaRate += 1
+                elif piles[pile] <= mid:
+                    bananaRate += 1
+            if bananaRate > h:
+                i = mid +1
             else:
-                i = mid+1
-        return minimumKElement
+                j = mid -1
+                minRate = min(minRate, mid)
+        return minRate
             
             
             
-        
-        
