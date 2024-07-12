41c93a4590b4e442403fda45178435a96902a3c7
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-07-12 01:44:00 -0700

    Time: 85 ms (65.57%), Space: 18.2 MB (88.22%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,32 +1,38 @@
 class Solution:
-    
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
-        def detectCycle(curr, source):
-            visited[curr] = True
-            for node in adj[curr]:
-                if visited[node] == False:
-                    if detectCycle(node, curr):
-                        return True
-                    else:
-                        continue
-                elif visited[node] == True and node != source:
-                    return True
-        visited = [False]*n
-        adj = [[] for i in range(n)]
+        if len(edges) == 0 and n == 1:
+            return True
+        elif len(edges) == 0:
+            return False
+        adj = [[] for _ in range(n)]
         for i in edges:
             adj[i[0]].append(i[1])
             adj[i[1]].append(i[0])
-        source = 0
+        visited = [False]*n
+        visited[0] = True
+        def checkTree(node, source):
+            nodes = adj[node]
+            for i in range(len(nodes)):
+                if visited[nodes[i]] == False:
+                    visited[nodes[i]] = True 
+                    if checkTree(nodes[i], node) == False:
+                        return False
+                else:
+                    if nodes[i] != source:
+                        return False
+            return True
         counter = 0
+        isChecked = False
         for i in range(n):
             if visited[i] == False:
                 counter += 1
+                isChecked = checkTree(0, 0)
                 if counter >= 2:
-                    return False
-                if detectCycle(source, source):
-                    return False
-        return True
-        
-        
+                    break
+        if counter >= 2:
+            return False
+        return isChecked
+                    
+    
+            
         
