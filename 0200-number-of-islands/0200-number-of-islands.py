48e5fb4ef11f64a313963675d231e99a2dbc4c98
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-07-15 14:56:32 -0700

    Time: 323 ms (8.46%), Space: 24.1 MB (35.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,14 +4,6 @@ class Solution:
         ans = 0
         dir = [[0,1], [1, 0], [-1, 0], [0, -1]]
         queueList = deque()
-        def countIsland(i, j):
-            for d in dir:
-                x = d[0] + i
-                y = d[1] + j
-                if x in range(len(grid)) and y in range(len(grid[0])) and grid[x][y] == '1' and (x, y)  not in visited:
-                    visited.add((x, y))
-                    queueList.append([x, y])
-
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1' and (i, j) not in visited:
@@ -19,7 +11,12 @@ class Solution:
                     queueList.append([i, j])
                     while queueList:
                         element = queueList.popleft()
-                        countIsland(element[0], element[1])
+                        for d in dir:
+                            x = d[0] + element[0]
+                            y = d[1] + element[1]
+                            if x in range(len(grid)) and y in range(len(grid[0])) and grid[x][y] == '1' and (x, y)  not in visited:
+                                visited.add((x, y))
+                                queueList.append([x, y])
         return ans
 
 
