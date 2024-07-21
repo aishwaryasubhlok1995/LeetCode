913929a1b51fbd684a3961289180559ef032aa41
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-07-20 18:20:27 -0700

    Time: 7771 ms (5.00%), Space: 16.6 MB (44.30%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,8 +1,8 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        
-        def backtrack(i, j, counter, visited):
+        visited = set()
+        def backtrack(i, j, counter):
             if counter == len(word):
                 return True
             for d in dir:
@@ -10,14 +10,16 @@ class Solution:
                 column = j + d[1]
                 if row in range(len(board)) and column in range(len(board[0])) and (row, column) not in visited and board[row][column] == word[counter]:
                     visited.add((row, column))
-                    if backtrack(row, column, counter+1, visited) == True:
+                    if backtrack(row, column, counter+1) == True:
                         return True
                     visited.remove((row,column))
             
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if backtrack(i, j, 1, {(i,j)}) == True:
+                    visited.add((i,j))
+                    if backtrack(i, j, 1) == True:
                         return True
+                    visited.remove((i,j))
         return False
         
