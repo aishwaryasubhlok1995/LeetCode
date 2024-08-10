f7905c5c6da78d53fa5549ebe7d91d3a16aadadb
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-08-09 19:37:37 -0700

    Time: 30 ms (94.49%), Space: 16.4 MB (74.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,33 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: 
-        list3 = None
+        list3 = ListNode(-1)
+        head = list3
         if list1 == None and list2 == None:
-            return list3
-        def createNode(val, list3):
-            if list3 == None:
-                head = ListNode(val, None)
-            else:
-                head = list3
-                curr = head
-                while curr.next != None:
-                    curr = curr.next 
-                curr.next = ListNode(val, None)
-            return head
+            return None
         while list1 != None or list2 != None:
             if list1 == None:
-                list3 = createNode(list2.val, list3)
+                node = ListNode(list2.val)
                 list2 = list2.next 
             elif list2 == None:
-                list3 = createNode(list1.val, list3)
+                node = ListNode(list1.val)
                 list1 = list1.next
             elif (list1.val > list2.val):
-                list3 = createNode(list2.val, list3)
+                node = ListNode(list2.val)
                 list2 = list2.next 
             else: 
-                list3 = createNode(list1.val, list3)
+                node = ListNode(list1.val)
                 list1 = list1.next
-        return list3
+            list3.next = node
+            list3 = list3.next
+        return head.next
             
         
                 
