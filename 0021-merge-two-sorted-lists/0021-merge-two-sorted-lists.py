fd8c66295d8247a4eb91c39ff6d154cf1c6fb2da
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-08-09 19:27:14 -0700

    Time: 58 ms (6.97%), Space: 16.7 MB (29.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,50 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        head=None
-        while list1 or list2 is not None:
-            if list1 != None and list2 != None:
-                if list1.val <= list2.val:
-                    if head == None:
-                        list3 = ListNode(list1.val) 
-                        head = list3
-                    else:
-                        list3.next = ListNode(list1.val)  
-                        list3 = list3.next
-                    list1 = list1.next
-                else:
-                    if head == None:
-                        list3 = ListNode(list2.val) 
-                        head = list3
-                    else:
-                        list3.next = ListNode(list2.val)  
-                        list3 = list3.next
-                    list2 = list2.next
-            elif list1 == None:
-                if head == None:
-                    list3 = ListNode(list2.val) 
-                    head = list3
-                else:
-                    list3.next = ListNode(list2.val)  
-                    list3 = list3.next
-                list2 = list2.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: 
+        list3 = None
+        if list1 == None and list2 == None:
+            return list3
+        def createNode(val, list3):
+            if list3 == None:
+                head = ListNode(val, None)
             else:
-                if head == None:
-                    list3 = ListNode(list1.val) 
-                    head = list3
-                else:
-                    list3.next = ListNode(list1.val)  
-                    list3 = list3.next
+                head = list3
+                curr = head
+                while curr.next != None:
+                    curr = curr.next 
+                curr.next = ListNode(val, None)
+            return head
+        while list1 != None or list2 != None:
+            if list1 == None:
+                list3 = createNode(list2.val, list3)
+                list2 = list2.next 
+            elif list2 == None:
+                list3 = createNode(list1.val, list3)
                 list1 = list1.next
-        return head
+            elif (list1.val > list2.val):
+                list3 = createNode(list2.val, list3)
+                list2 = list2.next 
+            else: 
+                list3 = createNode(list1.val, list3)
+                list1 = list1.next
+        print(list3)
+        return list3
             
-            
+        
+                
+                
+                
