81be99f2a918e76dcf36d95203a777515f02d745
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-08-22 22:39:42 -0700

    Time: 57 ms (36.57%), Space: 16.6 MB (73.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,21 +2,21 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         subArray = []
         final = []
-        def calculateSum(i):
-            if sum(subArray) == target:
+        def calculateSum(i, total):
+            if total == target:
                 final.append(subArray.copy())
                 return
         
-            if sum(subArray) > target or i >= len(candidates):
+            if total > target or i >= len(candidates):
                 return 
             
             subArray.append(candidates[i])
-            calculateSum(i)
+            calculateSum(i, total + candidates[i])
             
             subArray.pop()
-            calculateSum(i+1)
+            calculateSum(i+1 ,total)
         
-        calculateSum(0) 
+        calculateSum(0, 0) 
         return final
         
         
