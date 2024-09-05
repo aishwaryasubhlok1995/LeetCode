4a77870d9328742d05caedca5299b10703d48877
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-09-05 01:47:57 -0700

    Time: 403 ms (49.11%), Space: 16.6 MB (73.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,25 +4,18 @@ class Solution:
         for curr in range(1, len(s)):
             prev = curr-1
             nextt = curr+1
-            while prev >= 0 and nextt < len(s):
-                if s[prev] == s[nextt]:
-                    if len(res) < nextt-prev+1:
-                        res = s[prev:nextt+1]
-                    prev = prev-1
-                    nextt = nextt +1
-                else:
-                    break
+            while prev >= 0 and nextt < len(s) and s[prev] == s[nextt]:
+                if len(res) < nextt-prev+1:
+                    res = s[prev:nextt+1]
+                prev = prev-1
+                nextt = nextt +1
             prev = curr-1
             nextt = curr
-            while prev >= 0 and nextt < len(s):
-                if s[prev] == s[nextt]:
-                    if len(res) < nextt-prev+1:
-                        res = s[prev:nextt+1]
-                    prev = prev-1
-                    nextt = nextt +1
-                else:
-                    break
-               
+            while prev >= 0 and nextt < len(s) and s[prev] == s[nextt]:
+                if len(res) < nextt-prev+1:
+                    res = s[prev:nextt+1]
+                prev = prev-1
+                nextt = nextt +1
         return res
 
 
