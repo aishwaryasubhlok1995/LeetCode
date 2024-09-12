575fa0a6067606156e8461e05a7bc588bc493ad2
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-09-11 23:35:18 -0700

    Time: 650 ms (12.42%), Space: 23.5 MB (6.52%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,19 +1,18 @@
 class Solution:
     import heapq
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        ans = [] 
-        finalAns = []
-        heapq.heapify(ans)
-        def ksqrt(x, y):
-            return(-sqrt((y*y) + (x*x)))
+        pointsHeapList = []
+        ansList = []
         for i in points:
-            value = (ksqrt(i[0], i[1]))
-            if len(ans) < k:
-                heapq.heappush(ans, (value, i[0], i[1]))
-            else:
-                if ans[0][0] < value:
-                    heapq.heappop(ans)
-                    heapq.heappush(ans, (value, i[0], i[1]))
-        for i in range(len(ans)):
-            finalAns.append([ans[i][1], ans[i][2]])
-        return finalAns
+            value = i[0]*i[0] + i[1]*i[1]
+            heapq.heappush(pointsHeapList, [-value, i])
+            if len(pointsHeapList) > k:
+                 heapq.heappop(pointsHeapList)
+        print(pointsHeapList)
+        for i in pointsHeapList:
+            ansList.append(i[1])
+        return ansList
+            
+        
+            
+        
