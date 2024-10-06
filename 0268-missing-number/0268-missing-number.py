5d4867b933fd716e04e4386faa69e0f29b7657d1
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2024-10-05 21:28:37 -0700

    Time: 100 ms (95.58%), Space: 18.5 MB (5.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,15 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums = sorted(nums);
-        length = len(nums)
-        for i in range(length):
-            if i != nums[i]:
-                return i 
-        return length
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        counter = 0
+        nums = set(nums)
+        for i in range(len(nums)):
+            if counter in nums: 
+                counter += 1
+            else:
+                return counter
+        return counter
+            
+            
+        
+        
+        
