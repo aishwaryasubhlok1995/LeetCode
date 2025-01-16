cb1f7545f56760aa2cc85ee346172dd65f9c06b7
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-15 21:50:27 -0800

    Time: 74 ms (56.41%), Space: 23.5 MB (20.29%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.py
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.py
@@ -2,10 +2,9 @@ import random
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.w = w
         self.weight = []
         curr = 0
-        for i in self.w:
+        for i in w:
             curr += i
             self.weight.append(curr)
 
