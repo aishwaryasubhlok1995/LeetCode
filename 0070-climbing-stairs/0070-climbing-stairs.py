a761f0016f489c65ed521b4a2e50b92165251dbf
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-23 18:41:15 -0800

    Time: 0 ms (100%), Space: 17.7 MB (31.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        '''
+        we can solve dp with recursionw which causes 2^n and recursion with memization(hm) which give O(n) but space is also O(n) so we go with bottom up .. but with first, second, third 
+        '''
         memo = {}
         def helper(n):
             if n in memo:
