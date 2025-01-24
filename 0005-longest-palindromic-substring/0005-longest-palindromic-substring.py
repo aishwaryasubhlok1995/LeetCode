95f121886b6af64092cfa2c2b375ab3c8eeb5442
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-24 01:37:48 -0800

    Time: 315 ms (57.1%), Space: 17.7 MB (69.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,19 +6,20 @@ class Solution:
             left, right = i, i 
             ans = ""
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > maxLen:
+                    res = s[left:right+1]
+                    maxLen = len(res)
                 left -= 1
                 right += 1
-            ans = s[left+1:right]
-            right = i
-            left = i-1
+            
+            left = i
+            right = i+1
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > maxLen:
+                    res = s[left:right+1]
+                    maxLen = len(res)
                 right += 1
                 left -= 1
-            if len(ans) < (right - left):
-                ans = s[left+1:right] 
-            if len(ans) > maxLen:
-                maxLen = len(ans)
-                res = ans
         return res
             
 
