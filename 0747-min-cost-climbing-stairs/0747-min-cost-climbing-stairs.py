52be7aed4819484af74370454eba72cda9de444b
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-23 19:16:42 -0800

    Time: 4 ms (39.31%), Space: 17.7 MB (70.88%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,12 +1,11 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        arr = [math.inf]*(1+n)
-        for i in range(1, n+1):
-            if i == 1 or i == 2:
-                arr[i] = cost[i-1] 
-            else:
-                arr[i] =  cost[i-1] + min(arr[i-1], arr[i-2])
-        return min(arr[n], arr[n-1])
+        first =  cost[0]
+        second = cost[1]
+        for i in range(2, len(cost)):
+            third =  cost[i] + min(first, second)
+            first = second 
+            second = third
+        return min(second, first)
             
         
