663aaf3f24e5276519456d5375c7e476abd88841
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-26 15:51:36 -0800

    Time: 198 ms (18.17%), Space: 18.9 MB (35.07%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        heighest = max(piles)
+        minPiles = math.inf
+        if h == len(piles):
+            return heighest
+        lowest = 1
+        while lowest <= heighest:
+            middle = (lowest + heighest)//2
+            sumNo = 0 
+            for i in range(len(piles)):
+                sumNo  += math.ceil(piles[i]/middle)
+                if  sumNo > h:
+                    break 
+            if sumNo <= h:
+                minPiles = min(minPiles, middle)
+                heighest = middle - 1
+            elif sumNo > h:
+                lowest = middle + 1 
+        return minPiles
+
+            
