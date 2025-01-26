291777384bae09714fa8ab9d33e9d6baa123f393
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-25 16:48:41 -0800

    Time: 0 ms (100%), Space: 18 MB (64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,26 +1,28 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = 0
-        j = len(matrix)-1
-        while i<=j:
-            mid = (i+j)//2
-            if target>= matrix[mid][0] and target<= matrix[mid][-1]:
-                start = 0
-                end = len(matrix[0]) - 1
-                while start<=end:
-                    middle = (start+end)//2
-                    print(start)
-                    print(end)
-                    print(middle)
-                    if target == matrix[mid][middle]:
+        start = 0
+        end = len(matrix) - 1
+        while start <= end:
+            middle = (start + end)//2
+            if target <= matrix[middle][-1] and target >= matrix[middle][0]:
+                left = 0
+                right = len(matrix[0]) - 1
+                while left <= right:
+                    mid = (left+right)//2
+                    if matrix[middle][mid] == target:
                         return True
-                    if target < matrix[mid][middle]:
-                        end = middle -1
-                    elif target > matrix[mid][middle]:
-                        start = middle +1
+                    elif target < matrix[middle][mid]:
+                        right = mid -1 
+                    elif  target > matrix[middle][mid]:
+                        left = mid +  1
                 return False
-            elif target<matrix[mid][0]:
-                j = mid-1
-            elif target>matrix[mid][-1]:
-                i = mid+1
-        return False
+            if target < matrix[middle][0]:
+                end = middle - 1
+            elif target > matrix[middle][-1]:
+                start = middle + 1
+        return False 
+
+
+
+
+        
