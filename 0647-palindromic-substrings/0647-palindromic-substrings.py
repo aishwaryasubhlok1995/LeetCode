dc00900f0e4980e6529be6cddbe557615fb61b0d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-26 20:04:46 -0800

    Time: 127 ms (78.53%), Space: 17.8 MB (42.83%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -7,19 +7,12 @@ class Solution:
             left, right = i, i 
             ans = ""
             while left >= 0 and right < len(s) and s[left] == s[right]:                
-                if (right - left + 1) > maxLen:
-                    res = s[left:right+1]
-                    maxLen = len(res)
                 count += 1
                 left -= 1
                 right += 1
-            
             left = i
             right = i+1
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                if (right - left + 1) > maxLen:
-                    res = s[left:right+1]
-                    maxLen = len(res)
                 count += 1
                 right += 1
                 left -= 1
