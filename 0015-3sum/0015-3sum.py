79b0a2b2804b2179947e5776209e8d6750d3be1d
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-01-28 23:08:44 -0800

    Time: 1948 ms (6.13%), Space: 20.9 MB (21.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         tempSet = set()
-        resArray = []
         nums = sorted(nums)
         for i in range(len(nums)):
             valToSearch = nums[i]*-1
@@ -11,11 +10,11 @@ class Solution:
                 if nums[left] + nums[right] == valToSearch:
                     if (nums[left], nums[right], valToSearch*-1) not in tempSet:
                         tempSet.add((nums[left], nums[right], valToSearch*-1))
-                        resArray.append([nums[left], nums[right], valToSearch*-1])
+                        '''resArray.append([nums[left], nums[right], valToSearch*-1])'''
                     left += 1
                 elif nums[left] + nums[right] < valToSearch:
                     left += 1
                 else:
                     right -= 1
             
-        return resArray
+        return list(tempSet)
