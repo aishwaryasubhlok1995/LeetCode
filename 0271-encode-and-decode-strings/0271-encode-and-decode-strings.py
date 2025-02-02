e5bd9900e2f172fa442c30df799cd69594f3cf96
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-02-02 13:52:39 -0800

    Time: 144 ms (10.38%), Space: 18.3 MB (33.13%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,25 +2,29 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        ans = ''
-        for i in strs:
-            if i == '':
-                ans += 'None' + '[*/]' 
-            else:
-                ans += i + '[*/]'
-        return ans
-    
+        enStr = ''
+        for i in range(len(strs)):
+            strs[i] = strs[i].replace('*', '**')
+            enStr += strs[i] + '*:'
+        print(enStr)
+        return enStr
+
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
         ans = []
-        for i in (s.split('[*/]')):
-            if i == 'None':
-                ans.append("")
-            elif i != '':
-                ans.append(i)
-        return ans
+        j = 0
+        curr = ''
+        while j<len(s):
+            if s[j] != '*':
+                curr += s[j]
+                j += 1
+            else:
+                if s[j + 1] == '*':
+                    curr += '*'
+                else: 
+                    ans.append(curr)
+                    curr = ''
+                j += 2
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+        return ans
