d744fec4008acbdb86ca1fc23f65a05318e48149
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-02-13 02:57:46 -0800

    Time: 0 ms (100%), Space: 18.9 MB (42.83%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,4 +1,5 @@
 class Solution:
+# you are onl capturing when there is positive elevation, you don't need the day to sell, but if tday is #greater than yesterday it means u need to capture that, if there is deep then ignore, draw  graph , u  wull #understand 
     def maxProfit(self, prices: List[int]) -> int:
         ans = 0
         minNo = prices[0]
