575edc038249192d3f7b4cc6e13f9959e0be130c
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-02-25 21:39:28 -0800

    Time: 3 ms (58.67%), Space: 21.6 MB (5.56%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.py
+++ 0243-shortest-word-distance/0243-shortest-word-distance.py
@@ -1,14 +1,25 @@
 class Solution:
+
+    def preProcessing(self, wordsDict):
+        hm = {}
+        for i in range(len(wordsDict)):
+            if wordsDict[i] not in hm:
+                hm[wordsDict[i]] = []
+            hm[wordsDict[i]].append(i) 
+        return hm
+    
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         minDistance = math.inf
-        word1Idx = -1
-        word2Idx = -1 
-        for i in range(len(wordsDict)):
-            if wordsDict[i] == word1:
-                word1Idx = i 
-            elif wordsDict[i] == word2:
-                word2Idx = i 
-            if (word1Idx != -1 and word2Idx != -1):
-                minDistance= min(abs(word1Idx - word2Idx), minDistance)
+        dictWords = self.preProcessing(wordsDict)
+        array1 = dictWords[word1]
+        array2 = dictWords[word2]
+        i = 0 
+        j = 0 
+        while i<len(array1) and j<len(array2):
+            minDistance = min(minDistance, (abs(array1[i]-array2[j])))
+            if array1[i] < array2[j]:
+                i += 1
+            else:
+                j += 1
         return  minDistance
         
