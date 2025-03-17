0b565064bddc94f19ce1b034096adcd81d8a9126
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-03-16 20:39:59 -0700

    Time: 26 ms (87.44%), Space: 20.4 MB (70.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,20 +1,18 @@
 class Solution:
+    #this question is bit tricky but imagine, you have a branch and removal of some portion from it will give you K so we are checking how many occurrences of removal/wastage are there 
     def subarraySum(self, nums: List[int], k: int) -> int:
-        #watch neetcode for explanation like we are taking base case 0 
-        hm = {}
-        hm[0] = 1
-        currSum = 0
+        #base case hm = {0:1}
+        hm = {0:1}
+        runningSum = 0
         count = 0
-        for i in nums:
-            currSum += i 
-            if (currSum - k) in hm.keys():
-                count += hm[currSum - k]
-            if currSum in hm:
-                hm[currSum] += 1
+        for num in nums:
+            runningSum += num
+            if (runningSum - k) in hm:
+                count += hm[runningSum - k]
+            if runningSum not in hm:
+                hm[runningSum] = 1
             else:
-                hm[currSum] = 1
-            
-        return count 
-                
+                hm[runningSum] += 1
+        return count
             
         
