f7e4276c2f36574634600615670524d09fb8630c
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-03-23 18:21:55 -0700

    Time: 38 ms (19.36%), Space: 22.2 MB (18.77%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -0,0 +1,23 @@
+import random
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = nums[::]
+        self.temp = nums
+
+    def reset(self) -> List[int]:
+        return self.original 
+        
+    def shuffle(self) -> List[int]:
+        for i in range(len(self.temp)):
+            idx = random.randint(i, len(self.temp)-1)
+            self.temp[idx], self.temp[i] = self.temp[i], self.temp[idx]
+        return self.temp
+           
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
