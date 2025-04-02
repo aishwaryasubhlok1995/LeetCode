cc1aafa9761edc1b5d99f07adca53066f3a38a16
aishwaryasubhlok1995 <117240126+aishwaryasubhlok1995@users.noreply.github.com>
2025-04-02 13:49:51 -0700

    Time: 365 ms (17.55%), Space: 18.2 MB (21.77%) - LeetHub

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+class Solution:
+    def maxLength(self, arr: list[str]) -> int:
+        charSet = set()
+
+        def overlap(charSet, s):
+            c = Counter(charSet) + Counter(s)
+            return max(c.values()) > 1
+
+        def backtrack(i):
+            if i == len(arr):
+                return len(charSet)
+
+            res = 0
+            if not overlap(charSet, arr[i]):
+                for c in arr[i]:
+                    charSet.add(c)
+                res = backtrack(i + 1)
+                for c in arr[i]:
+                    charSet.remove(c)
+            return max(res, backtrack(i + 1))  # Don't concatenate arr[i]
+
+        return backtrack(0)
